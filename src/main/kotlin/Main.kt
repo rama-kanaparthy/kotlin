18c5efd9d1bb74582919d6925e54812ca9cdb976
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2025-04-10 21:05:47 +0530

    kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,21 @@
 package org.example
 
-fun main() {
+fun moveZerosToEnd(arr: IntArray): IntArray {
+    val result = mutableListOf<Int>()
+    var zeroCount = 0
 
+    for (num in arr) {
+        if (num == 0) zeroCount++ else result.add(num)
+    }
+
+    repeat(zeroCount) { result.add(0) }
+    return result.toIntArray()
 }
+
+// Usage
+fun main() {
+    val arr = intArrayOf(0, 1, 0, 3, 12)
+    println(moveZerosToEnd(arr).joinToString()) // Output: 1, 3, 12, 0, 0
+}
+
 
